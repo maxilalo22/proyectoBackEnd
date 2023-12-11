fe7611e5693ff90d1c325c65143579f04f86cb3d
Maximiliano Venditti <maxvenditti94@gmail.com>
2023-12-11 14:50:41 -0300

    chat incorporado

--- OneDrive/Documentos/proyectoBack/src/main.js
+++ OneDrive/Documentos/proyectoBack/src/main.js
@@ -8,6 +8,8 @@ import {productRouter} from './routes/productRouter.js'
 import { cartRouter } from './routes/cartRouter.js'
 import { viewRouter } from './routes/viewRouter.js'
 import { realRouter } from './routes/realTimeRouter.js';
+import { chatRouter } from './routes/chatRouter.js';
+import { messageModel } from './DAO/models/message.model.js';
 
 
 const app = express()
@@ -29,17 +31,46 @@ app.use(productRouter);
 app.use(cartRouter);
 app.use('/', viewRouter);
 app.use('/realTimeProducts', realRouter);
+app.use('/chat', chatRouter)
 
 app.get('/home', (req, res) => {
     res.render('home');
 });
 
+
 io.on('connection', (socket) => {
     console.log('Cliente conectado!');
+
+    socket.on('setUsername', (username) => {
+        socket.username = username;
+        socket.broadcast.emit('userConnected', `${socket.username} se ha conectado!`);
+    });
+
     socket.on('updateProducts', (products) => {
         socket.emit('updateProducts', products);
     });
+
+    socket.on('message', async (data) => {
+        const newMessage = new messageModel({
+            user: data.user,
+            message: data.message,
+        });
+
+        try {
+            await newMessage.save();
+            console.log('Mensaje guardado en la base de datos:', newMessage);
+            const messagesFromDB = await messageModel.find();
+            io.emit('messageLogs', messagesFromDB);
+        } catch (error) {
+            console.error('Error al guardar el mensaje:', error);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('userDisconnected', `${socket.username} se ha desconectado!`);
+    });
 });
+
 
 const MONGODB_URL = "mongodb+srv://maxvenditti94:mendoza110@cluster0.zyyalew.mongodb.net/ecommerce";
 
